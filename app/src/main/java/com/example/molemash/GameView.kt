96e4f2e627ac3d79230fca470c3d65a9f8edbb06
sunbee <sanjay.bhatikar@gmail.com>
2023-06-22 12:50:30 -0500

    Payload

--- app/src/main/java/com/example/molemash/GameView.kt
+++ app/src/main/java/com/example/molemash/GameView.kt
@@ -1,17 +1,24 @@
 package com.example.molemash
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.Canvas
+import android.graphics.Rect
 import android.view.MotionEvent
 import android.view.View
 
 class GameView(context: Context) : View(context) {
     private val mole: Mole = Mole(context, this)
+    private val canvasBitmap: Bitmap = BitmapFactory.decodeResource(context.resources, R.drawable.mole_hole)
+
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         // Draw the background image
-
+        val srcRect = Rect(0, 0, canvasBitmap.width, canvasBitmap.height)
+        val dstRect = Rect(0, 0, canvas.width, canvas.height)
+        canvas.drawBitmap(canvasBitmap, srcRect, dstRect, null)
         // Draw the mole
         mole.draw(canvas)
     }
